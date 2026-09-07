4e0ed6a854b2bd04ffd85799b1168792ef3ae729
mayukh <cultmt.616god@gmail.com>
2026-09-07 18:56:40 +0530

    fix: use absolute callbackURL for Google OAuth

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/api/auth/google/callback'
+    callbackURL: (env.CLIENT_URL || 'http://localhost:5000') + '/api/auth/google/callback'
   }, async (accessToken, refreshToken, profile, done) => {
     try {
       let user = await User.findOne({ googleId: profile.id });
